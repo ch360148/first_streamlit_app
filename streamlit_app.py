8e974ec8bdff1429b352298b0b9e6eee51ca6c86
ch360148 <123763020+ch360148@users.noreply.github.com>
2023-01-27 14:39:50 -0600

    Update streamlit_app.py
    Updates with smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Dinner')
+streamlit.title('My Mom\'s New Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
@@ -8,4 +8,5 @@ streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
